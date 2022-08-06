e980b219724fc96cf5c465a6831754d591534de1
Nasser <nassern502@yahoo.com>
2022-08-06 18:49:26 +0200

    implement Footbal & Weather as classes to use them in deserilizer

--- src/main/java/de/exxcellent/challenge/Football.java
+++ src/main/java/de/exxcellent/challenge/Football.java
@@ -1,4 +1,28 @@
 package de.exxcellent.challenge;
 
+import java.util.Arrays;
+
 public class Football {
+    public String team;
+    public int games;
+    public int wins;
+    public int losses;
+    public int draws;
+    public int goals;
+    public int goalsAllowed;
+    public int points;
+
+    public Football() {
+
+    }
+    public Football(String team,int ...values) {
+        this.team = team;
+        this.games = values[0];
+        this.wins = values[1];
+        this.losses =  values[2];
+        this.draws =  values[3];
+        this.goals =  values[4];
+        this.goalsAllowed =  values[5];
+        this.points =  values[6];
+    }
 }
--- src/main/java/de/exxcellent/challenge/Weather.java
+++ src/main/java/de/exxcellent/challenge/Weather.java
@@ -1,4 +1,40 @@
 package de.exxcellent.challenge;
 
 public class Weather {
+
+    public float day;
+    public float mxT;
+    public float mnT;
+    public float avT;
+    public float avDP;
+    public float lHrP;
+    public float tPcpn;
+    public float pDir;
+    public float avSp;
+    public float dir;
+    public float mxS;
+    public float skyC;
+    public float mxR;
+    public float mn;
+    public float r_avSLP;
+    public Weather() {}
+    public Weather(float... values) {
+        this.day = values[0];
+        this.mxT = values[1];
+        this.mnT = values[2];
+        this.avT = values[3];
+        this.avDP = values[4];
+        this.lHrP = values[5];
+        this.tPcpn = values[6];
+        this.pDir = values[7];
+        this.avSp = values[8];
+        this.dir = values[9];
+        this.mxS = values[10];
+        this.skyC = values[11];
+        this.mxR = values[12];
+        this.mn = values[13];
+        this.r_avSLP = values[14];
+    }
+
+
 }
