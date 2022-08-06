0898990ee9b9a4adbb9420c447755700020a602d
Nasser <nassern502@yahoo.com>
2022-08-06 18:33:50 +0200

    Add Tests

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,6 +3,9 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -22,10 +25,27 @@ class AppTest {
     void aPointlessTest() {
         assertEquals("successful", successLabel, "My expectations were not met");
     }
+    @Test
+    void dayOfSmallestTemperature(){
+     int exepted = 14;
+     int result = 14;
+        assertEquals(exepted, result, "My expectations were not met");
+    }
+    @Test
+    void teamOfSmallestDifferrence(){
+        String exepted = "Aston_Villa";
+        String result = "Aston_Villa";
+        assertEquals(exepted, result, "My expectations were not met");
+    }
 
     @Test
     void runFootball() {
         App.main("--football", "football.csv");
     }
+    @Test
+    void runWeather() {
+        App.main("--weather", "weather.csv");
+    }
+
 
 }
