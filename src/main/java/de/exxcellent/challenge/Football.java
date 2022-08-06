9511061885a4135b42d155342bdc0c157f15a4d6
Nasser <nassern502@yahoo.com>
2022-08-06 18:55:12 +0200

    implement descrelizer method in Football class

--- src/main/java/de/exxcellent/challenge/Football.java
+++ src/main/java/de/exxcellent/challenge/Football.java
@@ -2,7 +2,7 @@ package de.exxcellent.challenge;
 
 import java.util.Arrays;
 
-public class Football {
+public class Football implements  CsvDescerlizer {
     public String team;
     public int games;
     public int wins;
@@ -24,5 +24,16 @@ public class Football {
         this.goals =  values[4];
         this.goalsAllowed =  values[5];
         this.points =  values[6];
+    }
+
+    @Override
+    public Football descrelizer(String line, String sperator) {
+        String [] dataArray= line.split(sperator);
+        String [] strValues =Arrays.copyOfRange(dataArray,1,dataArray.length);
+
+        String team = dataArray[0];
+        int[] intValues= Arrays.stream(strValues).mapToInt(Integer::parseInt).toArray();
+
+        return new Football(team,intValues);
     }
 }
